0fbc330ce5686f5e204a00d9ee4d0f7c2ea2606e
Kosar0111 <81988686+Kosar0111@users.noreply.github.com>
2021-09-30 22:48:59 +0300

    Add files via upload

---
+++ Task_sum.js
@@ -0,0 +1,13 @@
+//task #5
+let k = 2;
+let l = 3;
+let sum = k + l;
+let dif = k - l;
+if (k <= 1 && l >= 3) {
+    console.log(`${sum}`);
+    alert(`${sum}`)
+}
+else {
+    console.log(`${dif}`);
+    alert(`${dif}`)
+}
