fde739428b4d6cbd4661ec496bbfe321576b0bcc
Konstantin Loktiev <kosar0111@gmail.com>
2021-10-27 01:06:06 +0300

    new task

--- globalHW.js
+++ globalHW.js
@@ -30,12 +30,15 @@ console.log(unique(arr1));
 //// test
 let sort1 = arr1.sort();
 let dubli = function (num) {
+
     for (let i = 0; i < num.length; i++) {
+
         if (num[i] === num[i - 1]) {
             num.splice(i, 1);
             ;
         }
     }
+
     return num;
 };
 
@@ -47,10 +50,12 @@ let arr2 = [1, 2, 3, 4, 1, 2];
 let arr3 = arr2.sort();
 let sortArr = [];
 arr3.filter(function (value) {
+
     if (arr3.indexOf(value) === arr3.lastIndexOf(value)) {
         sortArr.push(value);
-    }
-})
+    };
+});
+
 console.log(sortArr)
 
 
@@ -67,12 +72,17 @@ let obj = {
     a: 1,
     b: 2
 };
-let array = Object.entries(obj);
-console.log(array);
+let array = () => {
+
+    return Object.entries(obj);
+}
+console.log(array(obj));
 
 // 
 let creatingArray = (object) => {
+
     let arra = [];
+
     for (let prop in object) {
         if (object.hasOwnProperty(prop)) {
             arra.push([prop, object[prop]])
@@ -81,3 +91,68 @@ let creatingArray = (object) => {
     return arra;
 }
 console.log(creatingArray(obj))
+
+
+
+// next 4
+let obj1 = { a: 1, b: 5, c: 10 };
+let oneProp = (prop, ...rest) => {
+    let objProp = { ...prop };
+
+    rest.forEach((item) => {
+        delete objProp[item];
+    });
+
+    return objProp;
+};
+console.log(oneProp(obj1, 'b', 'c'))
+
+
+// next... task
+
+let empty = (obj) => {
+
+    for (key of obj) {
+        if (key !== null || key !== undefined || key !== NaN)
+
+            return true;
+
+    } return false;
+};
+// 
+
+let objEmpty = (object1) => {
+    let objEmptyKey = Object.keys(object1);
+    if (objEmptyKey.length === 0) {
+
+        return true;
+    }
+
+    return false
+};
+
+// next... task
+
+
+
+/* let Xobj = (obj1, obj2) => {
+    let keyObj1 = Object.key(obj1);
+    let keyObj2 = Object.key(obj1);
+    if (keyObj1 === keyObj2)
+};
+
+console.log(Xobj({ a: 1, b: 2 }, { c: 1, b: 2 })) */
+
+// next... task.
+let addA = (size, value) => new Array(size).map(item => {
+    item === value
+});
+console.log(addA(3, 'a'));
+
+// next
+
+let filterArray = [0, 1, false, 2, undefined, '', 3, null];
+let res = filterArray.filter(item => item);
+
+console.log(res)
+
